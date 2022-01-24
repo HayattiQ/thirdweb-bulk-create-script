c9a60b8a4542984a0ea972dcacf889474570659b
gurujowa <gurujowa@gmail.com>
2022-01-24 11:15:15 +0000

    read csv function

--- app.ts
+++ app.ts
@@ -1,13 +1,14 @@
 
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { ethers } from "ethers";
-const fs = require("fs").promises;
-
+const fs = require("fs");
+const Papa = require("papaparse");
 require('dotenv').config();
 
 const PRIVATE_KEY = process.env["PRIVATE_KEY"] as string;
 const FOLDER = "./input/";
 const DROP_MODULE = "0xB0fad666FDbA4a3C4054cB20f593A2dc2aFA21C6";
+const CSV_PATH = "./input/metadata.csv";
 
 const wallet = new ethers.Wallet(PRIVATE_KEY, ethers.getDefaultProvider("https://rpc-mumbai.maticvigil.com"));
 const sdk = new ThirdwebSDK(wallet);
@@ -22,48 +23,62 @@ type Meta = {
     value: string;
     display_type?: string;
   }[];
-  image: any;
+  image: Buffer;
 }
 
-const makeMetaData = async (): Promise<Meta> => {
-  const buff = await fs.readFile(FOLDER + "6.png");
-  const metadata: Meta = {
-    name: "TestV7Nam",
-    description: "The description for the bundle drop.",
-    external_url: "https://soudan-nft.xyz/",
-    background_color: "FFFFFF",
-    attributes: [
-      {
-        "trait_type": "Body",
-        "value": "Beauty"
-      },
-      {
-        "trait_type": "Background",
-        "value": "Green"
-      },
-      {
-        "trait_type": "Hair",
-        "value": "longhair2"
-      },
-      {
-        "display_type": "date",
-        "trait_type": "birthday",
-        "value": "Feb 14th"
-      },
-    ],
-    image: buff,
+const convertToMetaRow = async (row: any): Promise<Meta> => {
+
+  const buff = await fs.promises.readFile(FOLDER + row.path);
+  let metadata: Meta = {
+    name: row.name,
+    description: row.description,
+    external_url: row.external_url,
+    background_color: row.background_color,
+    attributes: [],
+    image: buff as Buffer,
   };
 
+  for (let [key, value] of Object.entries(row)) {
+    if (!["name", "description", "external_url", "background_color", "path"].includes(key)) {
+      // @ts-ignore
+      metadata.attributes[key as string] = {
+        value: value as string,
+        trait_type: key as string,
+      };
+    }
+  };
+  return metadata;
+}
+
+const makeMetaData = async (result: { data: any[] }): Promise<Meta[]> => {
+  let metadata: Meta[] = [];
+  for (let row of result.data) {
+    metadata.push((await convertToMetaRow(row)));
+  };
   return metadata;
 }
 
 
-
+function readCsv(filepath: string): Promise<Meta[]> {
+  const file = fs.createReadStream(filepath)
+  return new Promise((resolve, reject) => {
+    Papa.parse(file, {
+      header: true,
+      complete(results: any) {
+        resolve(makeMetaData(results))
+      },
+      error(err: any) {
+        reject(err)
+      }
+    })
+  })
+}
 
 const main = async () => {
-  const metadata = await makeMetaData();
+  const metadata = await readCsv(CSV_PATH);
+  console.log(metadata);
   const drop = sdk.getDropModule(DROP_MODULE);
-  await drop.createBatch([metadata]);
+  await drop.createBatch(metadata);
 }
 
 main();
